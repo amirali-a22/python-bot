648be96f97dd891e989ab0b24ed005aaf758b045
amirali <amirali.arn.a22@gmail.com>
2023-09-09 10:43:31 +0330

    edit main function structure

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
 import os
 import json
+import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 BASE_DIR = os.path.dirname(__file__)
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 def read_secrets() -> dict:
     filename = os.path.join(BASE_DIR, 'secrets.json') 
@@ -20,10 +30,17 @@ TOKEN = secrets.get("TOKEN")
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-app = ApplicationBuilder().token(TOKEN).build()
+# app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("hello", hello))
+# app.add_handler(CommandHandler("hello", hello))
 
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("hello", hello))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    
 if __name__ == "__main__":
     print('bot polling')
-    app.run_polling()
+    main()
