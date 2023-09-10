fa816bd2d3a9eccf67f43445a5a9ee1ab67955c6
Amirali <amirali.arn.a22@gmail.com>
2023-09-10 11:07:09 +0330

    add .vscode to gitignore and add buttons add add some functionality for buttons

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 import json
+import sys
 import logging
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackContext, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 BASE_DIR = os.path.dirname(__file__)
 # Enable logging
@@ -26,19 +28,70 @@ def read_secrets() -> dict:
     
 secrets = read_secrets()
 TOKEN = secrets.get("TOKEN")
+# def error(update: Update, context: CallbackContext):
+#     """Log Errors caused by Updates."""
+#     sys.stderr.write(f"ERROR: '{context.error}' caused by '{update}'")
+#     pass
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [
+        InlineKeyboardButton("wallet", callback_data='1'),
+        InlineKeyboardButton("buy config", callback_data='2')
+        ], 
+    [InlineKeyboardButton("test free config", callback_data='3')],
+    [
+        InlineKeyboardButton("invite link", callback_data='4'),
+        InlineKeyboardButton("my services", callback_data='5'),
+     ],
+    [
+        InlineKeyboardButton("guide", callback_data='6'),
+        InlineKeyboardButton("support", callback_data='7'),
+     ],
+    [InlineKeyboardButton("roles", callback_data='8')],
 
-# app = ApplicationBuilder().token(TOKEN).build()
+    ]
 
-# app.add_handler(CommandHandler("hello", hello))
+    # creating a reply markup of inline keyboard options
+    # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.inlinekeyboardmarkup.html
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    chat_id = update.message.id
+    # sending the message to the current chat id
+    # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.message.html#telegram.Message.reply_text
+    await update.message.reply_text(f'Hi {update.effective_user.first_name}')
+    await update.message.reply_text('Please choose:', reply_markup=reply_markup, reply_to_message_id=chat_id)
+
+
+async def button(update, context):
+    """
+    callback method handling button press
+    """
+    # getting the callback query
+    # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html
+    query: CallbackQuery = update.callback_query
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html#telegram.CallbackQuery.answer
+    await query.answer()
+
+    # editing message sent by the bot
+    # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html#telegram.CallbackQuery.edit_message_text
+    if query.data == '1':
+        # await query.edit_message_text(text=f"hey, fuck you: {query.data}")
+        await query.edit_message_text(text=f"hey, fuck you: {query.data}")
+    else:
+        await query.message.reply_text(text=f"Selected option: {query.data}")
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
-    application.add_handler(CommandHandler("hello", hello))
+
+    application.add_handler(CommandHandler("start", start))
+    # application.add_error_handler(error)  # error handling
+    application.add_handler(CallbackQueryHandler(button))  
+    # application.add_handler(CallbackQueryHandler(test, pattern='^5_test'))  
+
     application.run_polling(allowed_updates=Update.ALL_TYPES)
     
 if __name__ == "__main__":
